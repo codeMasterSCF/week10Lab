1fa0f3fee0d49eeb299c73fb25235504d1f88a76
463849 <463849@ELF6IT215756.ACDM.DS.SAIT.CA>
2017-11-15 14:33:40 -0700

    Created Week 10 Lab Created lab and completed it by creating filter

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,72 @@
+package filters;
+
+import businesslogic.UserService;
+import domainmodel.Role;
+import domainmodel.User;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author 463849
+ */
+public class AdminFilter implements Filter {
+    
+    private FilterConfig filterConfig = null;
+    
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        try {
+            // this code executes before the servlet
+            // ...
+            
+            // ensure user is authenticated
+            HttpSession session = ((HttpServletRequest)request).getSession();
+            UserService us = new UserService();
+            String username = (String) session.getAttribute("username");
+            
+            
+            User user = us.get(username);
+            Role role = user.getRole();
+            
+            
+            if (role.getRoleID() == 1) {
+                // yes, go onwards to the servlet or next filter
+                chain.doFilter(request, response);
+            } else {
+                // get out of here!
+                ((HttpServletResponse)response).sendRedirect("home");
+            }
+            
+            // this code executes after the servlet
+            // ...
+        } catch (Exception ex) {
+            Logger.getLogger(AdminFilter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    public void destroy() {        
+    }
+
+    public void init(FilterConfig filterConfig) {        
+        this.filterConfig = filterConfig;
+        
+    }
+    
+}
