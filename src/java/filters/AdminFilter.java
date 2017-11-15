94b5b255487863fc08ae5a26a1e4409aaa551010
463849 <463849@ELF6IT215756.ACDM.DS.SAIT.CA>
2017-11-15 14:44:58 -0700

    Modified Filter Made code more efficient

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,8 +1,6 @@
 package filters;
 
 import businesslogic.UserService;
-import domainmodel.Role;
-import domainmodel.User;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
